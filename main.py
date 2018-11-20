72655bb1e486612f74978797ceec2596c18e3ce3
Berkan <bhiziroglu14@ku.edu.tr>
2018-11-20 16:23:01 -0500

    Dataset helper functions

--- main.py
+++ main.py
@@ -2,8 +2,75 @@ import tensorflow as tf
 import numpy as np
 import math
 
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+#y_train
+#array([6, 9, 9, ..., 4, 9, 3])
+
+def load_pickle(f):
+    version = platform.python_version_tuple()
+    if version[0] == '2':
+        return  pickle.load(f)
+    elif version[0] == '3':
+        return  pickle.load(f, encoding='latin1')
+    raise ValueError("invalid python version: {}".format(version))
+
+def load_CIFAR_batch(filename):
+    """ load single batch of cifar """
+    with open(filename, 'rb') as f:
+        datadict = load_pickle(f)
+        X = datadict['data']
+        Y = datadict['labels']
+        X = X.reshape(10000, 3, 32, 32).transpose(0,2,3,1).astype("float")
+        Y = np.array(Y)
+        return X, Y
+
+def load_CIFAR10(ROOT):
+    """ load all of cifar """
+    xs = []
+    ys = []
+    for b in range(1,6):
+        f = os.path.join(ROOT, 'data_batch_%d' % (b, ))
+        X, Y = load_CIFAR_batch(f)
+        xs.append(X)
+        ys.append(Y)
+    Xtr = np.concatenate(xs)
+    Ytr = np.concatenate(ys)
+    del X, Y
+    Xte, Yte = load_CIFAR_batch(os.path.join(ROOT, 'test_batch'))
+    return Xtr, Ytr, Xte, Yte
+
+    
+def get_CIFAR10_data(num_training=49000, num_validation=1000, num_test=10000):
+    # Load the raw CIFAR-10 data
+    cifar10_dir = './cifar-10-batches-py'
+    X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
+    # Subsample the data
+    mask = range(num_training, num_training + num_validation)
+    X_val = X_train[mask]
+    y_val = y_train[mask]
+    mask = range(num_training)
+    X_train = X_train[mask]
+    y_train = y_train[mask]
+    mask = range(num_test)
+    X_test = X_test[mask]
+    y_test = y_test[mask]
+    # SUBTRACTING MEAN INCREASE ACCURACY ???? TRY OUT ???
+    # Normalize the data: subtract the mean image # SUBTRACTING MEAN INCREASE ACCURACY ???? TRY OUT ???
+    #mean_image = np.mean(X_train, axis=0)
+    #X_train -= mean_image
+    #X_val -= mean_image
+    #X_test -= mean_image
+    return X_train, y_train, X_val, y_val, X_test, y_test
+
+
+
+
 # define net
 class CifarNet():
+
     def __init__(self):
         # conv layer
         # H2 = (H1 - F + 2P)/S +1
@@ -152,3 +219,5 @@ class CifarNet():
                 plt.ylabel('minibatch loss')
                 plt.show()
         return total_loss, total_correct
+
+
